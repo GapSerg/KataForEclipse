4c09287376dc927df58f721a87c009b489f3208f
GapSerg <gapserg@tut.by>
2022-06-13 13:15:55 +0200

    Class for check initial data and Test fo this class

--- src/main/java/DataValid.java
+++ src/main/java/DataValid.java
@@ -1,15 +1,86 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DataValid {
     private boolean validator1(String someString){
         return someString.matches("(\\-?\\d+)|((\\-?\\d+)([,\n]{1}\\-?\\d+)+)");
     }
+    private String convertStringWithMinus (String someString){
+        StringBuilder rezult = new StringBuilder();
+        String[] temp= someString.split("\\-");
+        for (String s :temp) {
+            if (s.isEmpty()){
+                rezult.append("-");
+            }
+            else rezult.append(s).append(",");
+
+        }
+           rezult.deleteCharAt(rezult.length()-1);
+        return rezult.toString();
+    }
     public boolean validator(String someString){
+        String splitter;
         if (!someString.startsWith("//"))  {
             if (someString.isEmpty()){
                 return true;
             }
             return validator1(someString);
         }
-       return true;
+        else {
+            if (someString.substring(3,4).equals("\n")) {
+                splitter = someString.substring(2, 3);
+                
+                if ("([{\\^-=$!|]})?*+.".contains(splitter)){
+                    splitter="\\"+splitter;
+                }
+                 if (splitter.equals("\\-")){
+                     if (someString.charAt(someString.length()-1)=='-'){
+                         return false;
+                     }
+                     return validator1(convertStringWithMinus(someString.substring(4)));
+                 }
+                return validator1(someString.substring(4).replaceAll(splitter, ","));
+            }
+            else{
+
+                String[] partOfSomeString = someString.split("\n",2);
+                if (!partOfSomeString[0].matches("/{2}(\\[.+?\\])+")){
+                    return false;
+                }
+                else {
+                    Pattern pattern = Pattern.compile("\\[.+?\\]");
+                    Matcher matcher = pattern.matcher(partOfSomeString[0]);
+                    List<String> splitters=new ArrayList<>();
+                    while (matcher.find()) {
+                        splitters.add(partOfSomeString[0].substring(matcher.start()+1, matcher.end()-1));
+
+                    }
+                    for(int i=0;i<splitters.size();i++){
+                        if ("([{\\^-=$!|]})?*+.".contains(splitters.get(i))){
+                            splitters.set(i,"\\"+splitters.get(i));
+                        }
+                        if (splitters.get(i).equals("\\-")){
+                            if (someString.charAt(someString.length()-1)=='-'){
+                                return false;
+                            }
+                            partOfSomeString[1]=convertStringWithMinus(partOfSomeString[1]);
+                        }
+                        else partOfSomeString[1] = partOfSomeString[1].replaceAll(splitters.get(i), ",");
+                    }
+                    return validator1(partOfSomeString[1]);
+                }
+
+
+            }
+
+
+
+
+        }
+
+
     }
 
 }
--- src/test/java/DataValidTest.java
+++ src/test/java/DataValidTest.java
@@ -38,6 +38,42 @@ public class DataValidTest {
         Assert.assertTrue( dataValid.validator(dataTest.input8));
 
     }
+    @Test
+    public void anyElementStep4() {
+
+        Assert.assertTrue(dataValid.validator(dataTest.input9));
+        Assert.assertTrue(dataValid.validator(dataTest.input10));
+        Assert.assertTrue(dataValid.validator(dataTest.input11));
+        Assert.assertTrue(dataValid.validator(dataTest.input12));
+        Assert.assertFalse( dataValid.validator("//:/n\n1\n2:4"));
+        Assert.assertFalse( dataValid.validator("//:\n1:2::4"));
+        Assert.assertFalse( dataValid.validator("///:\n1:2:4"));
+
+    }
+        @Test
+        public void anyElementWithNegative() {
+        Assert.assertTrue( dataValid.validator(dataTest.input13));
+        Assert.assertTrue( dataValid.validator(dataTest.input14));
+        Assert.assertTrue( dataValid.validator(dataTest.input15));
+        Assert.assertTrue( dataValid.validator(dataTest.input16));
+        Assert.assertTrue( dataValid.validator(dataTest.input17));
+        Assert.assertFalse( dataValid.validator("//-\n10-3--2-"));
+        Assert.assertFalse( dataValid.validator("//-\n10-3---2"));
+        Assert.assertFalse( dataValid.validator("//-\n--10-3---2"));
+
+    }
+    @Test
+    public void anyElementManyAndMultDelit() {
+        Assert.assertTrue( dataValid.validator(dataTest.input18));
+        Assert.assertTrue( dataValid.validator(dataTest.input19));
+        Assert.assertTrue( dataValid.validator(dataTest.input20));
+        Assert.assertTrue( dataValid.validator(dataTest.input21));
+        Assert.assertTrue( dataValid.validator(dataTest.input22));
+        Assert.assertFalse( dataValid.validator("//[::][-][%%]\n12:::10-1003%%1\n2"));
+        Assert.assertFalse( dataValid.validator("//[:][-][%]\n12:10-1003%1%%2"));
+        Assert.assertFalse( dataValid.validator("//[:][--][%]\n-12:10-1003%1--2"));
+
+    }
 
 
 
